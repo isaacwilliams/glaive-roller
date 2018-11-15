babb0d7ee8efc825258d75c625202a360278da68
Isaac Williams <isaac@isaacwilliams.net>
2018-11-15 17:30:24 +1100

    Updates

---
+++ precache-manifest.e348d4e11cc8a986589e4c826772afd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43d4d1e8d2bde99e4d48",
+    "url": "/glaive-roller/static/js/runtime~main.43d4d1e8.js"
+  },
+  {
+    "revision": "9ac6f8b898b844ec9027",
+    "url": "/glaive-roller/static/js/main.9ac6f8b8.chunk.js"
+  },
+  {
+    "revision": "01b6dc214cc7b6a75744",
+    "url": "/glaive-roller/static/js/1.01b6dc21.chunk.js"
+  },
+  {
+    "revision": "9ac6f8b898b844ec9027",
+    "url": "/glaive-roller/static/css/main.c65c0398.chunk.css"
+  },
+  {
+    "revision": "2f0fe801b48d4758180c3d6b6da58b5f",
+    "url": "/glaive-roller/index.html"
+  }
+];
